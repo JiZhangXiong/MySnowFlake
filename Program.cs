c2a659795d36d0d060259cfb96ae5f71f0161fdd
jizhangxiong <jizhangxiong@hytch.com>
2023-12-28 11:02:49 +0800

    #修改方法

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MySnowFlake;
+
+Console.WriteLine($"雪花id：{SnowFlake.LongId}");
+
+Console.ReadLine();
